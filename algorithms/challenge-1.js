68c328572c0d82e5f4185fc4d915289ef4c6aecd
jayte054 <54476858+jayte054@users.noreply.github.com>
2022-06-23 15:07:17 +0100

    the idea is to ensure if the letters and the number of said letters in 2 strings are the same

--- algorithms/challenge-1.js
+++ algorithms/challenge-1.js
@@ -1,4 +1,10 @@
 function isAnagram(str1, str2) {
- 
+let isAnagram = function (stri,str2) {
+ let sorted1 =str1.split('').sort().join('').toLowerCase();
+ let sorted2 =str2.split('').sort().join('').toLowerCase();
+ return (sorted1 === sorted2);
 }
+}   
+console.log(isAnagram('hello', 'jello'));
+console.log(isAnagran('gate', 'tage'));
 module.exports = isAnagram
